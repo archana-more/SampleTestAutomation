ac70c684fbb7dcf3bda6066a42bf2ae942573326
ArchanaM3 <ArchanaM3@IMC6BCP107-MSL2.mumbai1.corp.citiustech.com>
2023-05-24 19:31:46 +0530

    First commit.

---
+++ SampleTestAutomation/src/test/java/Test/HelloTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.junit.Test;
+
+public class HelloTest {
+	
+	@Test
+	public void testMethod() {
+		System.out.println("Hello I am in HelloTest");
+	}
+
+	
+}
---
+++ SampleTestAutomation/src/test/java/Test/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package Test;
+
+import org.junit.Test;
+
+public class HelloWorldTest{
+	
+	@Test
+	public void secondTest() {
+		System.out.println("This is the hello world");
+	}
+	
+}
